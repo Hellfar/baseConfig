ace69a5054253455f6ffdff9e9e14e8378a1fbbc
pierre.l <Hellfar>
2016-09-23 16:21:29 +0200

    ruby install.

--- install.sh
+++ install.sh
@@ -21,8 +21,32 @@ fi
       "npm")
         sudo npm -g install "$soft"
         ;;
+      "gem")
+        gem install "$soft"
+        ;;
     esac
   fi
 done
 
 # rm -rf install.sh install_script
+
+# screen logger
+printf <<INITLOGGER
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          screenlogger
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: screenlogger
+# Description:       screenlogger
+### END INIT INFO
+
+if [ -e "$HOME/.bin/screenlogger.sh" ]
+then
+  ~/.bin/screenlogger.sh
+fi
+INITLOGGER | sudo tee /etc/init.d/screenlogger
+sudo chmod ugo+x /etc/init.d/screenlogger
+sudo update-rc.d screenlogger defaults

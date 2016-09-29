38f9bbe71b6e128a1426ea99d96182cfb404b7d1
pierre.l <pierre.l@snapkin.fr>
2016-09-29 17:12:04 +0200

    default parameter screenlogger.

--- .bin/screenlogger.sh
+++ .bin/screenlogger.sh
@@ -2,22 +2,28 @@
 
 # https://superuser.com/questions/662974/logging-lock-screen-events#answer-832111
 
+if [ -z "$1" ]
+then
+  export LOGFILE="~/log/lock_screen.log"
+else
+  export LOGFILE="$1"
+fi
+
 export SCREENSAVERSERVICE="org.mate.ScreenSaver"
 
 exit_report(){
-echo "$(date) Monitoring Terminated."
+  echo "$(date) Monitoring Terminated."
 }
 trap "exit_report; exit;" 0
 
 lockmon() {
-adddate() {
+  adddate() {
     while IFS= read -r line; do
       echo "$(date) $line" | grep "boolean" | sed 's/   boolean true/Screen Locked/' | sed 's/   boolean false/Screen Unlocked/'
     done
+  }
+  echo "$(date) Monitoring Started."
+  dbus-monitor --session "type='signal',interface='$SCREENSAVERSERVICE'" | adddate
 }
-echo "$(date) Monitoring Started."
-dbus-monitor --session "type='signal',interface='$SCREENSAVERSERVICE'" | adddate
 
-}
-
-lockmon >> ~/log/lock_screen.log
+lockmon >> "$LOGFILE"

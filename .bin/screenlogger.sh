f99517f81d3200dcb237b2f4780f09b1c9859ae8
pierre.l <pierre.l@snapkin.fr>
2016-09-30 11:11:00 +0200

    cleanner logs.

--- .bin/screenlogger.sh
+++ .bin/screenlogger.sh
@@ -12,17 +12,17 @@ fi
 export SCREENSAVERSERVICE="org.mate.ScreenSaver"
 
 exit_report(){
-  echo "$(date) Monitoring Terminated."
+  printf "$(date)\tMonitoring Terminated"
 }
 trap "exit_report; exit;" 0
 
 lockmon() {
   adddate() {
     while IFS= read -r line; do
-      echo "$(date) $line" | grep "boolean" | sed 's/   boolean true/Screen Locked/' | sed 's/   boolean false/Screen Unlocked/'
+      printf "$(date)\t$line" | grep "boolean" | sed 's/   boolean true/Screen Locked/' | sed 's/   boolean false/Screen Unlocked/'
     done
   }
-  echo "$(date) Monitoring Started."
+  printf "$(date)\tMonitoring Started"
   dbus-monitor --session "type='signal',interface='$SCREENSAVERSERVICE'" | adddate
 }
 

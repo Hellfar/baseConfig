1f6f02f7d30f38e3eb3cf78ec698c09ea4dc264d
pierre.l <pierre.l@snapkin.fr>
2016-10-06 10:17:47 +0200

    data frame lock logger.

--- .bin/screenlogger_analyser.r
+++ .bin/screenlogger_analyser.r
@@ -1,6 +1,9 @@
-#!/usr/bin/env R
+#!/usr/bin/env Rscript
+
+lock_screen_log <- read.delim("~/log/lock_screen.log")
 
 data.frame(
   datetime=lock_screen_log[[1]],
-  timestamp=strptime(lock_screen_log[[1]], "%a %b %d %T CEST %Y")
+  lap=strptime(lock_screen_log[[1]], "%a %b %d %T CEST %Y"),
+  way=lock_screen_log[[2]]
 )

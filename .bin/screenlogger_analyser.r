97e5f3ce80bdb8729e3d03087ac602ad209d0564
pierre.l <pierre.l@snapkin.fr>
2016-11-21 19:43:38 +0100

    guake and redshift at start up.

--- .bin/screenlogger_analyser.r
+++ .bin/screenlogger_analyser.r
@@ -2,7 +2,7 @@
 
 lock_screen_log <- read.delim("~/log/lock_screen.log")
 
-data.frame(
+lock_screen_data <- data.frame(
   datetime=lock_screen_log[[1]],
   lap=strptime(lock_screen_log[[1]], "%a %b %d %T CEST %Y"),
   way=lock_screen_log[[2]]

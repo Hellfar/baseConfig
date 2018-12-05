02de48d5bdf7d31280794605a878ddf059e72ab6
Hellfar <pierre.louchet@gmail.com>
2018-12-06 00:11:20 +0100

    More interestingly plotting numbers.

--- Templates/R-lang.R
+++ Templates/R-lang.R
@@ -2,22 +2,57 @@
 
 if (!require("optparse", quietly = TRUE)) {
 	install.packages("optparse", repos="http://cran.rstudio.com/")
-	library("optparse")
+	if (!library("optparse")) {
+    stop("Couldn't load 'optparse'.")
+  }
 }
 
 parser <- OptionParser()
 parser <- add_option(parser, c("-v", "--verbose"), action="store_true",
-                default=TRUE, help="Print extra output [default]")
-parser <- add_option(parser, c("-q", "--quietly"), action="store_false",
-                    dest="verbose", help="Print little output")
-parser <- add_option(parser, c("-c", "--count"), type="integer", default=5,
-                help="Number of random normals to generate [default %default]",
-                metavar="number")
+                default=FALSE, help="Print extra output [default]")
+parser <- add_option(parser, c("-d", "--driver"),
+                default="x11", help="Output in a choosen driver instead")
+parser <- add_option(parser, c("-i", "--ifile"),
+                default="stdin", help="File to plot")
+parser <- add_option(parser, c("-o", "--ofile"),
+                default="Rplots", help="Plot to file")
+parser <- add_option(parser, c("-t", "--type"),
+                default="l", help="Type of plot (p -> points, l -> lines, o -> overplotted points and lines, b-c -> points (empty if \"c\") joined by lines), s-S -> stair steps, h -> histogram-like vertical lines, n -> does not produce any points or lines")
+parser <- add_option(parser, c("--xlab"),
+                default="x", help="Define label for absciss")
+parser <- add_option(parser, c("--ylab"),
+                default="y", help="Define label for ordinate")
+parser <- add_option(parser, c("-c", "--col"),
+                default="black", help="Define color")
 options <- parse_args(parser)
 
-x11()
+if (options$driver == "")
+  options$driver = "pdf"
 
-numbers <- scan("stdin")
-plot(1:length(numbers), numbers, type="l")
+if (options$driver == "x11") {
+  x11()
+} else {
+  # if (options$verbose)
+  #   write(str(get(options$driver)), stdout())
+  # dev.copy(match.fun(options$driver), options$ofile)
+  match.fun(options$driver)(options$ofile)
+}
 
-while (!is.null(dev.list())) Sys.sleep(.1)
+if (options$verbose)
+  write(str(options), stdout())
+
+y <- scan(file=options$ifile, quiet=TRUE)
+if (options$verbose) {
+  write(paste("Length: ", length(y)), stdout())
+  write("Array:", stdout())
+  write(y, stdout())
+}
+
+if (length(y) > 0)
+  plot (0:(length(y) - 1), y, type=options$type, xlab=options$xlab, ylab=options$ylab, col=options$col)
+
+if (options$driver == "x11") {
+  while (!is.null(dev.list())) Sys.sleep(1)
+} else {
+  dev.off()
+}

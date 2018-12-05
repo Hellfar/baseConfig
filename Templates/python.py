eca8016be516775b66155f29bd30a8816259eae4
Hellfar <pierre.louchet@gmail.com>
2018-12-05 23:59:48 +0100

    Python lang basic template with arguments.

--- Templates/python.py
+++ Templates/python.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python
 
-import os
-import sys
-import json
+import getopt, sys
+
+# If it requires to be on a specific version of Python:
+# if sys.version_info >= (3, 0):
+#     sys.stdout.write("Sorry, requires Python 2.x, not Python 3.x\n")
+#     sys.exit(1)
+
+def main():
+    print 'Number of arguments:', len(sys.argv), 'arguments.'
+    print 'Argument List:', str(sys.argv)
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err)  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    output = None
+    verbose = False
+    for o, a in opts:
+        if o == "-v":
+            verbose = True
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-o", "--output"):
+            output = a
+        else:
+            assert False, "unhandled option"
+    # ...
+
+if __name__ == "__main__":
+    main()

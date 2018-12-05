d8021b39a84f2da9f882c797a6a3c0187c62033b
Hellfar <pierre.louchet@gmail.com>
2018-12-06 00:30:19 +0100

    Ruby lang basic template handling errors in call parameters.

--- Templates/ruby.rb
+++ Templates/ruby.rb
@@ -1,18 +1,46 @@
 #!/usr/bin/env ruby
 
+RATIO = 0.001
+
 if __FILE__ == $0
 
   require 'optparse'
 
-  options = {}
-  OptionParser.new do |opts|
-    opts.banner = "Usage: #{$0} [options]"
+  begin
+    ARGV << '-h' if ARGV.empty?
 
-    opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-      options[:verbose] = v
+    options = {
+      ratio: RATIO
+    }
+    optionparser = OptionParser.new do |opts|
+      opts.banner = "Usage: #{$0} [options]"
+
+      opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+        options[:verbose] = v
+      end
+      opts.on("-r", "--ratio RATIO", Integer, "Set the ratio (#{RATIO} by default)") do |r|
+        options[:ratio] = r
+      end
+
+      # opts.on_tail("-h", "--help", "Show this message") do
+      #   puts opts
+      #   exit
+      # end
     end
-  end.parse!
+    optionparser.parse!
+  rescue  OptionParser::InvalidOption,
+          OptionParser::MissingArgument,
+          OptionParser::InvalidArgument => error
+    puts error
+    puts optionparser.help
+    exit 2
+  end
 
   argv = ARGV.select{true}
+
+  if options[:verbose]
+    p options
+    p argv
+  end
 
 end
